759732a35e0f58bb03f572b48e517476ef682980
Marine136 <pedro.melo.rocha@hotmail.com>
2021-02-28 19:47:36 +0000

    primeiro commit

---
+++ app/src/main/java/ipvc/estg/cityhelper/MainActivity.kt
@@ -0,0 +1,15 @@
+package ipvc.estg.cityhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, R.string.welcome_alert, Toast.LENGTH_SHORT).show()
+
+    //change something
+    }
+}
